3b2bb1912dac5451a489054728d50ad716b1afd1
tuyengabi <tuyen.gabi@gmail.com>
2019-04-26 15:16:10 +0200

    add component and intance

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// THE COMPONENTS 
+
+Vue.component ('attack', {
+  data: function() {
+    return {
+      countMonster: 100,
+      countYou: 100
+    }
+  },
+  template:
+  `<section>
+  <div class="player">
+  <p>MONSTER</p>
+  <div class="lifeBar">
+  <p>{{ countMonster }}</p>
+  </div>
+  </div>
+  <div class="player">
+  <p>YOU</p>
+  <div class="lifeBar">
+  <p>{{ countYou }}</p>
+  </div>
+  </div>
+
+  <button
+  v-on:click="countMonster-=_.random(3,14)"
+  v-on:click="countYou-=_.random(2,15)"
+  >
+  ATTACK
+  </button>
+
+  <button
+  v-on:click="countMonster-=_.random(10,30)"
+  v-on:click="countYou-=_.random(10,30)"
+  >
+  SPECIAL ATTACK
+  </button>
+
+  <button
+  v-on:click="countYou+=10"
+  >
+  HEAL
+  </button>
+  </section>`
+})
+
+
+// THE INSTANCES //
+
+new Vue ({
+  el:'.game'
+})
+
