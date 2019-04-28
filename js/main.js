56290a2d83f72008c221dee5554a785e2131933d
tuyengabi <tuyen.gabi@gmail.com>
2019-04-28 14:12:24 +0200

    rename component and add button restart to file main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // THE COMPONENTS 
 
-Vue.component ('attack', {
+Vue.component ('slayer', {
   data: function() {
     return {
       countMonster: 100,
@@ -9,39 +9,47 @@ Vue.component ('attack', {
   },
   template:
   `<section>
-  <div class="player">
-  <p>MONSTER</p>
-  <div class="lifeBar">
-  <p>{{ countMonster }}</p>
-  </div>
-  </div>
-  <div class="player">
-  <p>YOU</p>
-  <div class="lifeBar">
-  <p>{{ countYou }}</p>
-  </div>
-  </div>
+     <div class="player">
+       <p>MONSTER</p>
+       <div class="lifeBar">
+         <p>{{ countMonster }}</p>
+       </div>
+     </div>
+     <div class="player">
+       <p>YOU</p>
+       <div class="lifeBar">
+         <p>{{ countYou }}</p>
+       </div>
+     </div>
 
-  <button
-  v-on:click="countMonster-=_.random(3,14)"
-  v-on:click="countYou-=_.random(2,15)"
-  >
-  ATTACK
-  </button>
+     <button
+       v-on:click="countMonster-=_.random(3,14)"
+       v-on:click="countYou-=_.random(2,15)"
+     >
+     ATTACK
+     </button>
 
-  <button
-  v-on:click="countMonster-=_.random(10,30)"
-  v-on:click="countYou-=_.random(10,30)"
-  >
-  SPECIAL ATTACK
-  </button>
+     <button
+       v-on:click="countMonster-=_.random(10,30)"
+       v-on:click="countYou-=_.random(10,30)"
+     >
+     SPECIAL ATTACK
+     </button>
 
-  <button
-  v-on:click="countYou+=10"
-  >
-  HEAL
-  </button>
+     <button
+       v-on:click="countYou+=10"
+     >
+     HEAL
+     </button>
+
+     <button
+       v-on:click="countMonster=100"
+       v-on:click="countYou=100"
+     >
+     RESTART
+     </button>
   </section>`
+
 })
 
 
